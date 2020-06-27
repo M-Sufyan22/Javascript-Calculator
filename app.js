75736261b647d8384638861644546d39af3013b4
Muhammad Sufyan <64953164+M-Sufyan22@users.noreply.github.com>
2020-06-27 23:40:37 +0500

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-function getnums(num) {
-    var result = document.getElementById("result");
-    result.value += num;
-}
-
-function clearall() {
-    var result = document.getElementById("result")
-    result.value = '';
-}
