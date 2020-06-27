44cc69cb6949db3458b790a701e847528462af03
M-Sufyan22 <mmuhammadsufyan5@gmail.com>
2020-06-27 21:40:22 +0500

    work uploaded

---
+++ app.js
@@ -0,0 +1,9 @@
+function getnums(num) {
+    var result = document.getElementById("result");
+    result.value += num;
+}
+
+function clearall() {
+    var result = document.getElementById("result")
+    result.value = '';
+}
