ec9cd47c2157ce88adafebd829a84d2f50846862
Muhammad Sufyan <64953164+M-Sufyan22@users.noreply.github.com>
2020-06-28 21:43:35 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 function getnums(val) {
-    document.getElementById("result").value += val;
+    var x = document.getElementById("result").value;
+    if (x == 0) {
+        document.getElementById("result").value = val;
+    } else {
+        document.getElementById("result").value += val;
+    }
 }
 
 function solve() {
-    let x = document.getElementById("result").value;
-    let y = eval(x);
+    var x = document.getElementById("result").value;
+    var y = eval(x);
     document.getElementById("result").value = y;
 }
 
 
 function clearall() {
-    document.getElementById("result").value = "";
+    document.getElementById("result").value = "0";
+}
+
+function deleteone() {
+    var x = document.getElementById("result").value;
+    if (x != 0) {
+        var z = document.getElementById("result").value.slice(0, -1);
+        document.getElementById("result").value = z;
+    }
+
 }
